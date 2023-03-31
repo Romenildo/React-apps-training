c29fc97768884162e36b6ab98a24a8fe8d66d341
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-03-31 16:53:48 -0300

    gerenciando input do form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,31 @@
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
+import { useState } from "react";
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('')
+    const [enteredAge, setEnteredAge] = useState('')
 
     const addUserHandler = event =>{
         event.preventDefault();//cancelar o reload do submit
+        console.log(enteredAge, enteredUsername)
+    }
+
+    const userNameChangeHandler = event =>{
+        setEnteredUsername(event.target.value)
+    }
+    const ageChangeHandler = event =>{
+        setEnteredAge(event.target.value)
     }
 
     return (
         <Card className ={classes.input}>
             <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="text" />
+                <label htmlFor="username" >Username</label>
+                <input id="username" type="text" onChange={userNameChangeHandler}/>
+                <label htmlFor="age" >Age (Years)</label>
+                <input id="age" type="text" onChange={ageChangeHandler}/>
                 <Button type="submit"> Add User</Button>
             </form>
         </Card>
