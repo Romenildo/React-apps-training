0f335360ba57b1dc9663347ac901894e931ce214
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-03-31 17:00:02 -0300

    validacao e reset form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,9 +9,22 @@ const AddUser = props => {
 
     const addUserHandler = event =>{
         event.preventDefault();//cancelar o reload do submit
-        console.log(enteredAge, enteredUsername)
+        
+        //validacoes
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            return
+        }
+        //o + é como definimos no useState como '', talvez der erro, e o + tranforma o valor em um numero
+        if(+enteredAge < 1){
+            return
+        }
+
+        //redefinindo os valores do input
+        setEnteredUsername('')
+        setEnteredAge('')
     }
 
+    //alterar os valores do input dinamicamente ao digitar
     const userNameChangeHandler = event =>{
         setEnteredUsername(event.target.value)
     }
@@ -23,9 +36,18 @@ const AddUser = props => {
         <Card className ={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username" >Username</label>
-                <input id="username" type="text" onChange={userNameChangeHandler}/>
+                <input 
+                    id="username" 
+                    type="text" 
+                    value={enteredUsername}//esse valor é necessario na hora de resetar os valores
+                                            //caso contrario seria necessario algum evento da funcao do set
+                    onChange={userNameChangeHandler}/>
                 <label htmlFor="age" >Age (Years)</label>
-                <input id="age" type="text" onChange={ageChangeHandler}/>
+                <input 
+                    id="age" 
+                    type="text" 
+                    value={enteredAge}
+                    onChange={ageChangeHandler}/>
                 <Button type="submit"> Add User</Button>
             </form>
         </Card>
