96585dfeac6cdba1ae1715c60f8abf2d92567f8b
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-03-31 16:14:30 -0300

    formulario inicial

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,20 @@
+
+
+const AddUser = props => {
+
+    const addUserHandler = event =>{
+        event.preventDefault();//cancelar o reload do submit
+    }
+
+    return (
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="text" />
+            <button type="submit"> Add User</button>
+        </form>
+    );
+}
+
+export default AddUser
